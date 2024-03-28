19062c180cfce03532886b0d61fc27a22464373b
muhammad usman <usmanqaisrani110@gmail.com>
2024-03-28 16:56:08 +0500

    task

---
+++ Day27/app79.js
@@ -0,0 +1,8 @@
+/*79: Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. Then, show how you can access the model property of the car.
+Explain & TIP: Objects are collections of properties, kind of like a box for storing related information. You can create an object to group related data and access its properties using either dot notation or bracket notation.*/
+var car = {
+    brand: "Toyota",
+    model: "Supra",
+    year: 2012
+};
+console.log(car.model);
---
+++ Day27/app80.js
@@ -0,0 +1,11 @@
+/*80: Updating Object Properties: Add a property named color to the existing car object, and then update the year property to 2021. Show how to perform these operations.
+Explain & TIP: You can add new properties to an object or change existing ones after the object is created. This flexibility allows objects to be dynamically updated as needed.*/
+var car2 = {
+    brand: "Honda",
+    model: "Civic",
+    year: 2020
+};
+//adding property
+car2.color = "Red";
+car2.year = "2022";
+console.log(car2);
