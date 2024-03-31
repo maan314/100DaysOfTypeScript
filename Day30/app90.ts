c74940de9c1aca7dd2fd751d5e4bd2949ca967ab
muhammad usman <usmanqaisrani110@gmail.com>
2024-03-31 19:17:37 +0500

    task

---
+++ Day30/app90.ts
@@ -0,0 +1,8 @@
+/*90: Checking if a Value is NaN: Demonstrate how to check if a variable's value is NaN (Not a Number) and return a boolean result.
+Explain & TIP: isNaN() helps you figure out if something that's supposed to be a number didn't turn out to be a number. It's useful for validating inputs or results of calculations.*/
+
+function isNanValue(value:any):boolean{
+    return isNaN(value);
+}
+console.log(isNanValue("Welcome"));
+console.log(isNanValue(123));
