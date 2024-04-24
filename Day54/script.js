c3703d4587b470de5a4c14b702f46ed66cc84527
muhammad usman <usmanqaisrani110@gmail.com>
2024-04-24 17:00:18 +0500

    task

---
+++ Day54/script.js
@@ -0,0 +1,10 @@
+//External script
+alert("Hello World external")
+//how we can declare varaible
+//let keyword
+let message = "Hello! How are you all ?"
+alert(message);
+//with const
+const name = "Usman";
+//UperCase constant
+const COLOR_RED ="#F00";
